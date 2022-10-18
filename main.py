686a3133e9c2d5659499756a5ce1583cc8237cf8
Stefan <stefan.holmberg@systementor.se>
2022-10-18 16:03:26 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,59 @@
+hemssida = "https://systementor.se"
+
+#if hemssida.startswith("http")
+
+
+team = " Hammarby "
+teamLower = team.lower()
+print(teamLower)
+
+print(team)
+print(team[0])
+print(team[1])
+print(team[7])
+
+
+
+namnet = input("Ange namn")
+print("Hej du heter " + namnet)
+
+print(len(namnet))
+
+
+
+# OM (len(namnet) == 0) 
+
+# print("50")
+# print(50)
+
+# print("Välkommen till spelet")
+# print('Välkommen till spelet')
+
+
+# print("It's Tuesday")
+# #print('It's Tuesday')
+# #"
+# print('Så här ser ett citationstecken ut " cdsd')
+
+# a = int("12") #Här görs sträng till int
+# s = str(12) # Görs int om till sträng
+
+
+# age = int(input("Ange din ålder:")) #"55"
+# add = int(input("Ange antal år du vill lägga till:")) #"10"
+# newAge = age  + add # "55 + "10" -> "5510"
+# namn = input("Skriv in ditt namn:")
+# print("Hej " + namn + " om " + str(add) + " år är du " + str(newAge) + " år")
+# # print("Hello")
+
+# # print("2012-10-12")
+# # print(2012 - 10- (12+3) * 2)
+
+# # print("Hello"  + " " +  "Stefan")
+# # print("Hello " + "Stefan")
+# # print("Hello" + " Stefan")
+# # TEXTER   "Hejsan"  strin g
+# # HELTAL  12
+# # FLYTTAL (float) eller decimaltal 3.14
+ 
+
